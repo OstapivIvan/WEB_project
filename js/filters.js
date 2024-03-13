9c300a905d24512835f2aa056556f1373334e37a
OstapivIvan <ostapivivan05@gmail.com>
2024-03-13 17:46:49 +0200

    Сhange the view of information about survey participants by placing the data in a table

--- js/filters.js
+++ js/filters.js
@@ -1,4 +1,5 @@
 const allParticipantsDiv = document.getElementById('allParticipantsList');
+
 document.addEventListener("DOMContentLoaded", function () {
     const participantsData = JSON.parse(localStorage.getItem('surveyData'));
 
@@ -14,11 +15,44 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function displayAllParticipants(participantsData) {
-    participantsData.forEach(participant => {
-        const participantDiv = document.createElement('div');
-        participantDiv.textContent = `Full Name: ${participant.fullName}, Phone: ${participant.phone}, Age: ${calculateAge(participant.dob)}, Position: ${participant.position}`;
-        allParticipantsDiv.appendChild(participantDiv);
+    const table = document.createElement('table');
+    table.id = "allParticipantsTable"
+    const caption = document.createElement('caption');
+    caption.textContent = 'Participants Information';
+    table.appendChild(caption);
+    const thead = document.createElement('thead');
+    const tbody = document.createElement('tbody');
+
+    const headers = ['Full Name', 'Phone', 'Age', 'Position', 'Foot', 'Height'];
+    // Створюємо рядок заголовка
+    const headerRow = document.createElement('tr');
+    headers.forEach(headerText => {
+        const header = document.createElement('th');
+        header.textContent = headerText;
+        headerRow.appendChild(header);
     });
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
+    // Додаємо дані про кожного учасника
+    participantsData.forEach(participant => {
+        const rowData = {
+            'Full Name': participant.fullName,
+            'Phone': participant.phone,
+            'Age': calculateAge(participant.dob),
+            'Position': participant.position,
+            'Foot': participant.foot,
+            'Height': participant.height
+        };
+        const row = document.createElement('tr');
+        headers.forEach(headerText => {
+            const cell = document.createElement('td');
+            cell.textContent = rowData[headerText];
+            row.appendChild(cell);
+        });
+        tbody.appendChild(row);
+    });
+    table.appendChild(tbody);
+    allParticipantsDiv.appendChild(table);
 }
 
 function calculateAge(dob) {
