16253d3afdc1f2a45ebda7fb8b9568d1e88cdc9f
OstapivIvan <ostapivivan05@gmail.com>
2024-03-16 13:55:34 +0200

    Add functions to control the movement of the game character using buttons for up, down, left, and right directions

--- js/gameScript.js
+++ js/gameScript.js
@@ -104,3 +104,24 @@ function everyinterval(n) {
     if ((myGameArea.frameNo / n) % 1 == 0) { return true; }
     return false;
 }
+
+function moveup() {
+    myGamePiece.speedY = -1;
+}
+
+function movedown() {
+    myGamePiece.speedY = 1;
+}
+
+function moveleft() {
+    myGamePiece.speedX = -1;
+}
+
+function moveright() {
+    myGamePiece.speedX = 1;
+}
+
+function clearmove() {
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;
+}
